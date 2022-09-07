572d1651ec11f9ad25a64b51a8d88c975b335edf
Shadow <113051326+python3shadow@users.noreply.github.com>
2022-09-07 21:38:06 +0200

    Update xspammer nuker.py

--- xspammer nuker.py
+++ xspammer nuker.py
@@ -1,3 +1,5 @@
-Heyy it's again shadow (but not the creator)
+print("Heyy it's again shadow (but not the creator)")
 
-xspammer : https://mega.nz/folder/5D8BAaRA#17vD5CTThD0X2-FFW6NyxA
+echo spammer : starting
+
+https://mega.nz/folder/5D8BAaRA#17vD5CTThD0X2-FFW6NyxA
