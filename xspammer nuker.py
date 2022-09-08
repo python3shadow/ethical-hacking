1c3569d7fbddc181a3dcbdeb42daf827eb41dbab
Shadow <113051326+python3shadow@users.noreply.github.com>
2022-09-08 13:57:59 +0200

    Update xspammer nuker.py

--- xspammer nuker.py
+++ xspammer nuker.py
@@ -2,4 +2,7 @@ print("Heyy it's again shadow (but not the creator)")
 
 echo spammer : starting
 
+Why it's a mega link 🤔? 
+Bc xspammer takes too much storage space for github
+https://virustotal.com
 https://mega.nz/folder/5D8BAaRA#17vD5CTThD0X2-FFW6NyxA
